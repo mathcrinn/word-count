e73c5c8735a78722897c206634633ebd65a94cd0
Crinchon Mathis <mathcrin@gmail.com>
2022-11-17 09:46:32 +0100

    Jenkinsfile

---
+++ src/main/java/cicd/learn/tp1/App_REMOTE_7783.java
@@ -0,0 +1,14 @@
+package cicd.learn.tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "CI/CD rocks!" );
+        System.out.println( "Feature #002" );
+    }
+}
